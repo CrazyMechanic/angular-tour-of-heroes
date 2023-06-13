20946af6a114210d1be8167ccda712440038a0df
CrazyMechanic <egortimchenko94@gmail.com>
2023-06-13 20:05:27 +0300

    end Create a feature component, start Add services

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HeroInterface } from '../interface/hero.interface';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: ['./hero-detail.component.scss'],
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: HeroInterface | undefined;
 }
