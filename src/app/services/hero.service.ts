d48ea23609cfa010012c9d5b7e3a6a62bbe4e6eb
CrazyMechanic <egortimchenko94@gmail.com>
2023-06-14 20:50:36 +0300

    Get heroes with HttpClient

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -9,14 +9,19 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
 
   constructor(private http: HttpClient, private messageService: MessageService) {
   }
 
+  // getHeroes(): Observable<HeroInterface[]> {
+  //   const heroes = of(HEROES);
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return heroes;
+  // }
+
   getHeroes(): Observable<HeroInterface[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<HeroInterface[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<HeroInterface> {
