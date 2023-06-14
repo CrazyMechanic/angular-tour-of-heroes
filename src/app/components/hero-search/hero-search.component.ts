21c1fb4108964c851314479b9f553921b320b0e6
CrazyMechanic <egortimchenko94@gmail.com>
2023-06-14 23:49:31 +0300

    AsyncPipe

--- src/app/components/hero-search/hero-search.component.ts
+++ src/app/components/hero-search/hero-search.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { HeroInterface } from '../../interface/hero.interface';
+import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
-  styleUrls: ['./hero-search.component.scss']
+  styleUrls: ['./hero-search.component.scss'],
 })
-export class HeroSearchComponent {
+export class HeroSearchComponent implements OnInit {
+  heroes$!: Observable<HeroInterface[]>;
+  private searchTerms = new Subject<string>();
 
+  constructor(private heroService: HeroService) {
+  }
+
+  search(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit() {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
+  }
 }
